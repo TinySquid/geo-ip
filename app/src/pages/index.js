9f8ce43931e1f7132b091611c6ab15379bdb7206
Michael Nunes <tinysquid.git@gmail.com>
2020-11-24 16:56:46 -0800

    Refactor page to use newly created hooks / funcs

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,5 +1,5 @@
 // React
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import axios from "axios";
 
@@ -12,7 +12,12 @@ import InfoPanel from "../components/InfoPanel/InfoPanel";
 import MapBox from "../components/MapBox/MapBox";
 import LiveMarker from "../components/MapBox/LiveMarker";
 
+// Hooks
+import { useSearchHistory } from "../hooks/useSearchHistory";
+
+// Utils
 import { daysBetween } from "../utility/dateRange";
+import { parseFromSearchHistory } from "../utility/parse";
 
 // SC
 import styled from "styled-components";
@@ -48,142 +53,77 @@ const Title = styled.h1`
 `;
 
 export default function Home() {
-  const [localIp, setLocalIp] = useState(null);
-  const [searchResult, setSearchResult] = useState({
-    data: [
-      { header: "ip address", text: "192.212.174.101" },
-      { header: "location", text: "Brooklyn, NY 10001" },
-      { header: "timezone", text: "UTC -05:00" },
-      { header: "isp", text: "SpaceX Starlink" },
-    ],
-    latlon: [51.505, -0.09],
-  });
+  const [findSearchItem, addSearchItem, deleteSearchItem] = useSearchHistory(
+    []
+  );
 
-  async function getLocalIpAddress() {
-    // Hits backend /ip endpoint to get our local ip
-    return axios.get(`${process.env.GATSBY_GET_LOCAL_IP_ENDPOINT}`);
-  }
+  const search = useCallback(
+    (ip) => {
+      // Will show ip location from cache or API if cache outdated or not found
+      const searchItem = findSearchItem(ip);
 
-  async function getIpLocationData(ipOrDomain) {
-    // Hits backend /location endpoint to get information about passed ip / domain
-    return axios.get(
-      `${process.env.GATSBY_GET_IP_LOCATION_ENDPOINT}?host=${ipOrDomain}`
-    );
-  }
+      if (searchItem) {
+        // Verify date
+        const today = new Date().setHours(0, 0, 0, 0);
 
-  function updateSearchHistory(search) {
-    const searchHistory = localStorage.getItem("cache");
+        if (daysBetween(new Date(searchItem.timestamp), today) < 7) {
+          // Use existing location information from history
+          const result = parseFromSearchHistory(searchItem);
 
-    if (searchHistory) {
-      // pre-existing cache
-      localStorage.setItem(
-        "cache",
-        JSON.stringify([...JSON.parse(searchHistory), search])
-      );
-    } else {
-      // First store to cache
-      localStorage.setItem("cache", JSON.stringify([search]));
-    }
-  }
-
-  function parseSearchResultFromAPI(data) {
-    // Don't need req flag in our localstorage...
-    delete data.success;
-
-    // Send to state
-    _searchResultHelper(data);
-
-    // Update cache since this is new data
-    updateSearchHistory(data);
-  }
-
-  function parseSearchResultFromCache(cacheResult) {
-    // Cache already in correct format to be displayed
-    _searchResultHelper(cacheResult);
-  }
-
-  function _searchResultHelper(search) {
-    // Used by the parse functions to set the search result state for the page. *DO NOT USE ON ITS OWN*
-    setSearchResult({
-      data: [
-        { header: "ip address", text: search.ip },
-        { header: "location", text: search.location },
-        { header: "timezone", text: search.timezone },
-        { header: "isp", text: search.isp },
-      ],
-      latlon: search.latlon,
-    });
-  }
-
-  // By default we want to get our local ip onMount
-  useEffect(() => {
-    // Gatsby build-time guard
-    if (typeof window !== "undefined" && window) {
-      (async () => {
-        const { data } = await getLocalIpAddress();
-
-        setLocalIp(data.req);
-      })();
-    }
-  }, []);
-
-  // Will determine if we grab location data from cache or API
-  useEffect(() => {
-    // Gatsby build-time guard
-    if (typeof window !== "undefined" && window) {
-      if (localIp) {
-        // Get cache
-        const searchHistory = JSON.parse(localStorage.getItem("cache"));
-
-        if (!searchHistory) {
-          // Get fresh location data and setup cache for next use
-          (async () => {
-            const { data } = await getIpLocationData(localIp);
-
-            parseSearchResultFromAPI(data);
-          })();
+          setState({ searchIp: ip, searchResult: result });
         } else {
-          // Verify cache search is not stale
-          const cachedResult = searchHistory.filter(
-            (search) => search.ip === localIp
-          );
-
-          if (cachedResult.length > 0) {
-            const today = new Date().setHours(0, 0, 0, 0);
-
-            if (daysBetween(new Date(cachedResult[0].timestamp), today) < 7) {
-              parseSearchResultFromCache(cachedResult[0]);
-            } else {
-              (async () => {
-                const { data } = await getIpLocationData(localIp);
-
-                parseSearchResultFromAPI(data);
-              })();
-            }
-          }
+          // Get fresh location information
+          console.log("Need API data");
         }
       }
-    }
-  }, [localIp]);
+    },
+    [findSearchItem]
+  );
+
+  const [state, setState] = useState({
+    searchIp: null,
+    searchResult: {
+      data: [
+        { header: "ip address", text: "8.8.8.8" },
+        { header: "location", text: "Mountain View, California 94043-1351" },
+        { header: "timezone", text: "UTC -08:00" },
+        { header: "isp", text: "Google LLC" },
+      ],
+      latlon: [37.4224, -122.08421],
+    },
+  });
+
+  useEffect(() => {
+    // Will get our local IP on initial page visit
+    axios.get(`${process.env.GATSBY_GET_LOCAL_IP_ENDPOINT}`).then((res) => {
+      const { data } = res;
+
+      if (data.success) {
+        search(data.req);
+      }
+    });
+  }, [search]);
+
+  if (!state.searchIp) return null;
 
   return (
     <Layout>
       <Container>
         <Header>
           <Title>IP Address Locator</Title>
-          <SearchBox initialValue={localIp} />
-          <InfoPanel data={searchResult.data} />
+          <SearchBox initialValue={state.searchIp} />
+          <InfoPanel data={state.searchResult.data} />
         </Header>
         <MapBox
           options={{
             style: { height: "calc(100vh - 300px)", width: "100vw" },
-            center: searchResult.latlon,
+            center: state.searchResult.latlon,
             zoom: 15,
             zoomControl: false,
             scrollWheelZoom: true,
           }}
         >
-          <LiveMarker position={searchResult.latlon} />
+          <LiveMarker position={state.searchResult.latlon} />
         </MapBox>
       </Container>
     </Layout>
