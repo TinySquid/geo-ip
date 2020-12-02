84645f5a300aa8f39f9e2507c8a0f63d90b27c7c
Michael Nunes <tinysquid.git@gmail.com>
2020-12-01 21:15:03 -0800

    Add parse function for API location response

--- app/src/utility/parse.js
+++ app/src/utility/parse.js
@@ -1,3 +1,9 @@
+/*
+  My reasoning for making these functions is to provide a single place 
+  for converting the API response object and search history object into 
+  the correct shape to be consumed by the page etc.
+*/
+
 function parseFromSearchHistory(data) {
   /*
     data should be an object that matches this:
@@ -32,4 +38,40 @@ function parseFromSearchHistory(data) {
   };
 }
 
-export { parseFromSearchHistory };
+function parseFromAPI(data) {
+  /*
+    data should be an object that matches this (from the API):
+      {
+        success: true,
+        ip: "8.8.8.8"​,
+        isp: "Google LLC",
+        latlon: [37.4224, -122.08421],
+        location: "Mountain View, California 94043-1351",
+        timezone: "UTC -8",
+        timestamp: 1606867200000
+      }
+
+    We want 2 objects as a result - one for the map and info panel, and the other for storing into browser localstorage.
+    The API already provides a response that fits the map + infopanel requirements, so we need to just make another object for localstorage
+  */
+
+  // To be consumed by page components
+  const pageObject = parseFromSearchHistory(data);
+
+  // For storing search into localstorage with the right shape
+  const storageObject = {
+    ip: data.ip,
+    isp: data.isp,
+    latlon: data.latlon,
+    location: data.location,
+    timezone: data.timezone,
+    timestamp: data.timestamp,
+  };
+
+  return {
+    pageObject,
+    storageObject,
+  };
+}
+
+export { parseFromSearchHistory, parseFromAPI };
